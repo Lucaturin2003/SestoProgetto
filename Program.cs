1b6aa00ec3a2bbfd8232649614881c7e3233b87b
Lucaturin2003 <159005233+Lucaturin2003@users.noreply.github.com>
2024-02-06 12:47:51 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Polymorphism
+{
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Shape(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public abstract void Draw();
+        public void Copy()
+        {
+            Console.WriteLine("Copied shape into clipboard");
+        }
+
+    }
+    public class Circle: Shape
+    {
+        public Circle(int width, int height): base(width, height){}
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle");
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public Rectangle(int width, int height): base(width, height){}
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a rectangle");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle = new Circle(100, 200);  
+            circle.Draw();
+
+            var rectangle = new Rectangle(200, 100);    
+            rectangle.Draw();
+
+        }
+    }
+}
